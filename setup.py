1573683d1db2e22bd34320795c0e183ca8ac3ee4
illoRocks <illoRocks@gmail.com>
2018-02-08 22:22:19 +0700

    add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+   
 setup(
     name='stockscreener',
     version='1.0.0',
     packages=[''],
     url='https://illo.rocks',
     license='Creative Commons Attribution-NonCommercial-ShareAlike 2.0 Germany License',
+    long_description=long_description,
     author='Oliver Haag',
     author_email='stockscreener@illo.rocks',
     description='Download XBRL from the SEC and store it to MongoDB or as local files.', 
