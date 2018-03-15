755d884fb07ad0002121894ef6a476fe6aec54f6
illoRocks <oliver_haag@gmx.net>
2018-03-15 22:37:36 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 from distutils.core import setup
 
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
-   
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+VERSION = find_version('torchvision', '__init__.py')
+
+LONG_DESCRIPTION = open("README.md", 'r').read()
+
+REQUIREMENTS = [
+    'requests',
+    'pymongo'
+]
+
 setup(
     name='stockscreener',
-    version='1.0.0',
-    packages=[''],
-    url='https://illo.rocks',
+    version=VERSION,
+    packages=['stockscreener'],
+    author='Oliver Haag',
+    url='https://github.com/illoRocks/stockscreener',
     license='Creative Commons Attribution-NonCommercial-ShareAlike 2.0 Germany License',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     author='Oliver Haag',
     author_email='stockscreener@illo.rocks',
-    description='Download XBRL from the SEC and store it to MongoDB or as local files.', 
-    requires=['requests', 'pymongo']
+    description='Download XBRL from the SEC and store it to MongoDB or as local files.',
+    requires=REQUIREMENTS,
+    license='Creative Commons'
 )
