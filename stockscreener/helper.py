0a5bb18d5f8be535b9ea9d1729d6c50937cac000
illoRocks <oliver_haag@gmx.net>
2019-04-17 22:52:52 +0200

    singleton helper for metaclass

--- stockscreener/helper.py
+++ stockscreener/helper.py
@@ -58,3 +58,10 @@ def parse_date(str):
 
     format = '%Y-%m-%d'
     return datetime.strptime(str, format)
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
